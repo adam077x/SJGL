35a6b6403bba23f4d5fdf257e361ae2a1cffb8ed
adam077x <adam.heczko@gmail.com>
2018-11-11 17:07:07 +0100

    Quick fix

--- src/sjgl/object/entity/Entity.java
+++ src/sjgl/object/entity/Entity.java
@@ -7,13 +7,15 @@ import sjgl.vectors.Vector2f;
 
 public abstract class Entity implements GameObject {
 
-	protected Vector2 position, size;
-	protected Vector2f velocity;
+	public Vector2 position, size;
+	public Vector2f velocity;
+	public String entityName;
 	
-	public Entity(Vector2 position, Vector2 size) {
+	public Entity(Vector2 position, Vector2 size, String entityName) {
 		this.position = position;
 		this.size = size;
 		velocity = new Vector2f(0, 0);
+		this.entityName = entityName;
 	}
 	
 	@Override
